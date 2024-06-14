5e5ec3c6d4ef64b3580f0fed3b7cc321b6dc1f3e
kharism <kharisma@kanosolution.com>
2024-06-14 19:00:55 +0700

    refactor bgchangeevent

--- core/bgchangeevent.go
+++ core/bgchangeevent.go
@@ -21,22 +21,26 @@ type BGChangeEvent struct {
 	Shader *ebiten.Shader
 }
 
-func NewBGChangeEventFromPath(imagepath string, moveParam MoveParam, imageloader ImageLoader, shader *ShaderParam) Event {
-	image, _ := imageloader.GetImage(imagepath)
+func NewBgChangeEvent(img *ebiten.Image, moveParam MoveParam, shader *ShaderParam) Event {
 	if shader == nil {
-		return &BGChangeEvent{bg: image, Shader: nil, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
+		return &BGChangeEvent{bg: img, Shader: nil, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
 	}
-
 	if shader.Shader != nil {
-		return &BGChangeEvent{bg: image, Shader: shader.Shader, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
+		return &BGChangeEvent{bg: img, Shader: shader.Shader, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
 	} else {
 		sh, _ := GetShaderPool().GetShader(shader.ShaderName)
-		return &BGChangeEvent{bg: image, Shader: sh, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
+		return &BGChangeEvent{bg: img, Shader: sh, Sx: moveParam.Sx, Sy: moveParam.Sy, Tx: moveParam.Tx, Ty: moveParam.Ty, Speed: moveParam.Speed}
 	}
+}
+func NewBGChangeEventFromPath(imagepath string, moveParam MoveParam, imageloader ImageLoader, shader *ShaderParam) Event {
+	image, _ := imageloader.GetImage(imagepath)
+	return NewBgChangeEvent(image, moveParam, shader)
 
 }
 func (b *BGChangeEvent) Execute(scene *Scene) {
 	img := MovableImage{image: b.bg, mutex: &sync.Mutex{}, Shader: b.Shader}
+	img.x = b.Sx
+	img.y = b.Sy
 	moveAnim := MoveAnimation{
 		tx:    b.Tx,
 		ty:    b.Ty,
