cf563ddf84a7a720f0de3c6a6b0c1b52c63269e0
kharism <kharisma@kanosolution.com>
2024-06-14 19:01:35 +0700

    add another character constructor

--- core/character.go
+++ core/character.go
@@ -1,12 +1,20 @@
 package core
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 type Character struct {
 	Name string
 	Img  *MovableImage
 }
 
+func NewCharacterImage(name string, image *ebiten.Image) *Character {
+	newChar := &Character{Name: name, Img: &MovableImage{image: image, mutex: &sync.Mutex{}}}
+	return newChar
+}
 func NewCharacter(name, imgpath string, loader ImageLoader) *Character {
 	newChar := &Character{Name: name}
 	if imgpath != "" {
